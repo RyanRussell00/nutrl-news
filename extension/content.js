4002469f7e6c88c84eae1477f3437ff7f281293a
Ryan Russell <ryan.russell2000@gmail.com>
2020-10-17 21:02:53 -0700

    bruv it worsksss

--- extension/content.js
+++ extension/content.js
@@ -4,19 +4,40 @@ var supportedSites = ["Fox News", "CNN", "Donald J. Trump", "Joe Biden"];
 
 var indexOfNewsArticles = [];
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-sleep(5000).then(() => {
-  console.log("Start injection");
+console.log("Start injection");
+
+var theActualFeedElement = document.querySelector('[role="feed"]');
+
+sleep(2000).then(() => {
+  theActualFeedElement.addEventListener(
+    "DOMSubtreeModified",
+    contentChanged,
+    false
+  );
+});
+
+var lastFeedIndex = 0; // Index used for pagination
+var lastMetadataIndex = 0;
+
+var metadataElements = [];
+
+// Function to run when the feed gets updated with more content
+function contentChanged() {
+  theActualFeedElement.removeEventListener(
+    "DOMSubtreeModified",
+    contentChanged,
+    false
+  );
+
+  //   console.log("feed changed!");
 
   // Get total number of div inside main feed
   var feedElement = document.querySelectorAll(
     '[data-pagelet^="FeedUnit_"] [id^="jsc_c_"]'
   );
-  var metadataElements = [];
-  var last = 0;
-  for (i = 0; i < feedElement.length; i++) {
+
+  // loop over feedelements
+  for (i = lastFeedIndex; i < feedElement.length; i++) {
     let e = feedElement[i];
     if (
       i >= 2 &&
@@ -27,61 +48,64 @@ sleep(5000).then(() => {
     }
     if (e.localName == "h4") {
       metadataElements.push(e);
-    //   last = i;
     }
   }
-  console.log("feed element");
-  console.log(feedElement);
-  console.log(metadataElements);
-  var lastIx = 0; // Index used for pagination
 
-  // Get the element that has the post author
-  //   var authorListElements = [];
+  lastFeedIndex = feedElement.length;
 
-  //   var captionListElements = [];
+  for (i = lastMetadataIndex; i < metadataElements.length; i += 2) {
+    let text = metadataElements[i].innerText;
+    // Not a supported site
+    // if (!supportedSites.includes(text)) {
+    //   continue;
+    // }
+    insertIntoFeed(Math.floor((i + 2) / 2));
+  }
 
-  //   // console.log(posts);
-  //   feedElement.addEventListener("DOMSubtreeModified", contentChanged, false);
+  lastMetadataIndex = metadataElements.length;
 
-  //   // Function to run when the feed gets updated with more content
-  //   function contentChanged() {
-  //     //   console.log("feed changed!");
+  //   sleep(10000).then(() => {
+  //     return;
+  //   });
+  theActualFeedElement.addEventListener(
+    "DOMSubtreeModified",
+    contentChanged,
+    false
+  );
+}
 
-  //     authorListElements = document.getElementsByClassName(
-  //       "oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl oo9gr5id gpro0wi8 lrazzd5p"
-  //     );
+function insertIntoFeed(index) {
+  //   console.log("insert index: " + index);
+  var feedUnitsElements = document.querySelectorAll(
+    '[data-pagelet^="FeedUnit_"]'
+  );
+  //   console.log(feedUnitsElements);
+  if (index >= feedUnitsElements.length) {
+    return;
+  }
+  feedUnitsElements[index].insertAdjacentHTML(
+    "afterend",
+    `<div style="width:100% !important;background-color: red !important;"><h1>HELLO</h1></div><br>`
+  );
+}
 
-  //     captionListElements = document.getElementsByClassName(
-  //       "d2edcug0 hpfvmrgz qv66sw1b c1et5uql gk29lw5a a8c37x1j keod5gw0 nxhoafnm aigsh9s9 d9wwppkn fe6kdd0r mau55g9w c8b282yb hrzyx87i jq4qci2q a3bd9o3v knj5qynh oo9gr5id hzawbc8m"
-  //     );
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-  //     //   console.log();
+// Get the element that has the post author
+//   var authorListElements = [];
 
-  //     for (i = lastIx; i < authorListElements.length; i++) {
-  //       let childrenAuthors = authorListElements[i].childNodes;
-  //       // let childrenCaptions = captionListElements[i].childNodes;
-  //       for (j = 0; j < childrenAuthors.length; j++) {
-  //         let authorElement = childrenAuthors[j];
-  //         //   let captionElement = childrenCaptions[j];
-  //         // Grab caption if author is what we want
-  //         if (
-  //           authorElement.innerText != null &&
-  //           authorElement.innerText != undefined &&
-  //           authorElement.innerText != ""
-  //         ) {
-  //           // showNewsArticle(authorElement, captionElement, i);
-  //         }
-  //       }
-  //     }
-  //     lastIx = authorListElements.length;
-  //   }
+//   var captionListElements = [];
 
-  //   function showNewsArticle(authorElement, captionElement, index) {
-  //     console.log(authorElement.innerText + " : : " + captionElement.innerText);
-  //     if (supportedSites.includes(authorElement.innerText) == false) {
-  //       return;
-  //     }
+//   // console.log(posts);
 
-  //     console.log("Supported Site: " + authorElement.innerText);
-  //   }
-});
+//   function showNewsArticle(authorElement, captionElement, index) {
+//     console.log(authorElement.innerText + " : : " + captionElement.innerText);
+//     if (supportedSites.includes(authorElement.innerText) == false) {
+//       return;
+//     }
+
+//     console.log("Supported Site: " + authorElement.innerText);
+
+// sleep(5000).then(() => {}
